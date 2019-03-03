4857c5668c920311494d38ddaea7fddc33b0329d
CodeAlien <danylo.hreshchuk@gmail.com>
2019-03-03 12:52:26 +0200

    refactoring Min steps in infinite grid

--- arrays/min_steps_in_infinite_grid/main.py
+++ arrays/min_steps_in_infinite_grid/main.py
@@ -1,21 +1,25 @@
-def calculateDistance(x1, x0):
-    if x1 < 0 and x0 < 0:
-        return abs(abs(x1) - abs(x0))
-    if x1 < 0:
-        return abs(x1) + x0
-    return abs(x1 - x0)
+def calculateDistance(finishPosition, startPosition):
+    if finishPosition < 0 and startPosition < 0:
+        return abs(abs(finishPosition) - abs(startPosition))
+    if finishPosition < 0:
+        return abs(finishPosition) + startPosition
+    return abs(finishPosition - startPosition)
 
 
 class Solution:
     def coverPoints(self, A, B):
         i = 0
-        j = 0
+        result = 0
         while (i < len(A) - 1):
             xDiff = calculateDistance(A[i + 1], A[i])
             yDiff = calculateDistance(B[i + 1], B[i])
             if xDiff > yDiff:
-                j += xDiff
+                result += xDiff
             else:
-                j += yDiff
+                result += yDiff
             i += 1
-        return j
+        return result
+
+if __name__ == '__main__':
+    s =  Solution()
+    print(s.coverPoints([1,3,-5,7], [4,4,7,4]))
